e8ee83f2e3cbb3896c2205a253a535038932c9e4
Jerry Wang <jerry.wang1@kaplan.com>
2015-08-12 11:45:44 -0400

    copied from vagrant branch in kong

---
+++ Vagrantfile
@@ -0,0 +1,33 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  config.vm.box = "precise64"
+  config.vm.box_url ="http://files.vagrantup.com/precise64.box"
+  config.vm.synced_folder "../kong", "/kong"
+  config.vm.synced_folder "./", "/kong/dev"
+  config.vm.provider :virtualbox do |vb|
+     vb.memory = 2048
+  end
+  config.vm.network :forwarded_port, guest: 8000, host: 8000
+  config.vm.network :forwarded_port, guest: 8001, host: 8001
+  config.vm.provision "shell", inline: "
+    apt-get update
+    apt-get install wget curl tar make gcc unzip git liblua5.1-0-dev luarocks netcat lua5.1 openssl libpcre3 libpcre3-dev openjdk-7-jdk libcurl4-openssl-dev pkg-config dnsmasq -y --force-yes
+    # Build dependencies for OpenResty.
+    apt-get install build-essential libpcre3-dev libssl-dev libgeoip-dev
+
+    # Install standard Nginx first so that you get the relevant service scripts installed too
+
+    # If you want to access Postgres via Nginx
+    apt-get install libpq-dev
+
+    echo 'nameserver 10.0.2.3' >> /etc/resolv.conf
+    echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+    echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
+    /kong/dev/setup.sh
+    "
+end
---
+++ setup.sh
@@ -0,0 +1,77 @@
+OUT=/tmp/build/out
+TMP=/tmp/build/tmp
+echo "Cleaning directories"
+rm -rf $OUT
+rm -rf $TMP
+echo "Preparing environment"
+mkdir -p $OUT
+mkdir -p $TMP
+
+git config --global url.'https://'.insteadOf git://
+
+OPENSSL_VERSION=1.0.2d
+OPENRESTY_VERSION=1.7.10.2
+OPENRESTY_CONFIGURE="--sbin-path=/usr/sbin/nginx \
+--conf-path=/etc/nginx/nginx.conf \
+--error-log-path=/var/log/nginx/error.log \
+--http-client-body-temp-path=/var/lib/nginx/body \
+--http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
+--http-log-path=/var/log/nginx/access.log \
+--http-proxy-temp-path=/var/lib/nginx/proxy \
+--http-scgi-temp-path=/var/lib/nginx/scgi \
+--http-uwsgi-temp-path=/var/lib/nginx/uwsgi \
+--lock-path=/var/lock/nginx.lock \
+--pid-path=/var/run/nginx.pid \
+--with-luajit \
+--with-http_dav_module \
+--with-http_flv_module \
+--with-http_geoip_module \
+--with-http_gzip_static_module \
+--with-http_realip_module \
+--with-http_stub_status_module \
+--with-http_ssl_module \
+--with-http_sub_module \
+--with-ipv6 \
+--with-sha1=/usr/include/openssl \
+--with-md5=/usr/include/openssl \
+--with-http_stub_status_module \
+--with-http_secure_link_module \
+--with-http_sub_module \
+--with-http_postgres_module"
+
+git config --global url.'https://'.insteadOf git://
+
+# Download OpenSSL
+cd $TMP
+wget -q https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz -O openssl-$OPENSSL_VERSION.tar.gz
+tar xzf openssl-$OPENSSL_VERSION.tar.gz
+if [ "$(uname)" = "Darwin" ]; then # Checking if OS X
+  export KERNEL_BITS=64 # This sets the right OpenSSL variable for OS X
+fi
+OPENRESTY_CONFIGURE="--with-openssl=$TMP/openssl-$OPENSSL_VERSION"
+
+
+############################################
+######### Install Patched OpenResty ########
+############################################
+cd $TMP
+wget -q http://openresty.org/download/ngx_openresty-$OPENRESTY_VERSION.tar.gz
+tar xzf ngx_openresty-$OPENRESTY_VERSION.tar.gz
+cd ngx_openresty-$OPENRESTY_VERSION
+# Download and apply nginx patch
+cd bundle/nginx-*
+wget -q https://raw.githubusercontent.com/openresty/lua-nginx-module/ssl-cert-by-lua/patches/nginx-ssl-cert.patch --no-check-certificate
+patch -p1 < nginx-ssl-cert.patch
+cd ..
+# Download `ssl-cert-by-lua` branch
+wget -q https://github.com/openresty/lua-nginx-module/archive/ssl-cert-by-lua.tar.gz -O ssl-cert-by-lua.tar.gz --no-check-certificate
+tar xzf ssl-cert-by-lua.tar.gz
+# Replace `ngx_lua-*` with `ssl-cert-by-lua` branch
+NGX_LUA=`ls | grep ngx_lua-*`
+rm -rf $NGX_LUA
+mv lua-nginx-module-ssl-cert-by-lua $NGX_LUA
+# Configure and install
+cd $TMP/ngx_openresty-$OPENRESTY_VERSION
+./configure ${OPENRESTY_CONFIGURE}
+make
+make install
